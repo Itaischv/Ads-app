efce0e1f238730b150d6a86f5bf23b9182a57502
Itai Schvartzbard <itai@practitest.com>
2023-10-05 17:10:26 +0300

    managing connection and table creation if needed

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,53 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import pg from 'pg';
+import pg from 'pg'
 import cors from 'cors';
 import 'dotenv/config';
 const app = express();
 const port = process.env.PORT || 3030;
-const { Pool } = pg;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 // PostgreSQL configuration
-const pool = new Pool({
+const client = new pg.Pool({
     user: process.env.PG_USER,
-    host: 'localhost',
-    database: process.env.PG_DB_NAME,
     password: process.env.PG_PWD,
-    port: 5432
+    host: 'localhost',
+    port: 5432,
+    database: process.env.PG_DB_NAME
 });
 
-// Endpoint to add an event
+
+const createEventsTable = async () => {
+    try {
+        await client.connect();
+        const query = `CREATE TABLE IF NOT EXISTS events (
+                                                   id SERIAL PRIMARY KEY,
+                                                   type VARCHAR(255) NOT NULL,
+                                                   timestamp TIMESTAMP NOT NULL,
+                                                   slot VARCHAR(255),
+                                                   googleQueryId VARCHAR(255),
+                                                   timeOnPage NUMERIC(10, 3)
+                           );`
+        await client.query(query);
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+createEventsTable().then(r => console.log(r));
+
+// Posting an event:
+
 app.post('/events', async (req, res) => {
     try {
         const { type, timestamp, slot, googleQueryId, timeOnPage } = req.body;
         const insertQuery = 'INSERT INTO events (type, timestamp, slot, googlequeryid, timeonpage) VALUES ($1, $2, $3, $4, $5) RETURNING *';
         const values = [type, timestamp, slot, googleQueryId, timeOnPage];
-        console.log("values", values)
-        const result = await pool.query(insertQuery, values);
+        const result = await client.query(insertQuery, values);
         res.json(result.rows[0]);
     } catch (error) {
         console.error('Error adding event:', error);
@@ -34,10 +55,11 @@ app.post('/events', async (req, res) => {
     }
 });
 
+// Fetch events:
 app.get('/events', async (req, res) => {
     try {
         const query = 'SELECT * FROM events ORDER BY id DESC LIMIT 20';
-        const result = await pool.query(query);
+        const result = await client.query(query);
         res.json(result.rows);
     } catch (error) {
         console.error('Error fetching events:', error);
